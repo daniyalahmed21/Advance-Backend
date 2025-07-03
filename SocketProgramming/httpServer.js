43ebcced803f4341cf7fa862ba64f577b1420f13
Daniyal Ahmed <da9033431@gmail.com>
2025-07-03 18:34:27 +0500

    http server

---
+++ SocketProgramming/httpServer.js
@@ -0,0 +1,13 @@
+const http = require("http")
+
+const server = http.createServer((req,res)=>{
+    if(req.url==="/home"){
+        res.end("Welcome to home page")
+    }else{
+        res.end("Welcome to website")
+    }
+})
+
+server.listen("3000",()=>{
+    console.log("New server up on port 3000");
+})
