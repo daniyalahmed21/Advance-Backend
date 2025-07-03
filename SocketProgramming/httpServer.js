2da27c669d5145de0a2bb1d2c26f57f7b226545c
Daniyal Ahmed <da9033431@gmail.com>
2025-07-03 18:37:08 +0500

    comments

--- SocketProgramming/httpServer.js
+++ SocketProgramming/httpServer.js
@@ -2,7 +2,7 @@ const http = require("http")
 
 const server = http.createServer((req,res)=>{
     if(req.url==="/home"){
-        res.end("Welcome to home page")
+        res.end("Welcome to home page") //Ends the response
     }else{
         res.end("Welcome to website")
     }
