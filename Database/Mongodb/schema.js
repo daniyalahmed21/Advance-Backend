7a5ca28ba9eb88c0edfabf7ead673a3b24213828
Daniyal Ahmed <da9033431@gmail.com>
2025-07-09 14:14:03 +0500

    modifying validation

--- Database/Mongodb/schema.js
+++ Database/Mongodb/schema.js
@@ -19,3 +19,37 @@ db.createCollection("Student",{
     validationAction : "error"
 }) ;
 
+db.runCommand({
+    collMod:"Student",
+    validator : {
+        $jsonSchema : {
+            required : ["name","age"],
+            properties : {
+                name : {
+                    bsonType : "string",
+                    description : "name must be string and required "
+                },
+                age : {
+                    bsonType : "number",
+                    description : "age must be number and required"
+                },
+                courses : {
+                    bsonType : "array",
+                    description : "courses must be an array",
+                    items : {
+                        bsonType : "object",
+                        required : ["courseId","courseName"],
+                        properties : {
+                            courseId : {
+                                bsonType : "number"
+                            },
+                            courseName : {
+                                bsonType : "string"
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    },
+})
