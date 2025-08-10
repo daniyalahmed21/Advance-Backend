c6dc821b553ed2eca09fb4419d11236ae28513cc
Daniyal Ahmed <da9033431@gmail.com>
2025-08-10 15:42:54 +0100

    billboard ui configured for monitoring queues

--- Typescript/dist/index.js
+++ Typescript/dist/index.js
@@ -8,11 +8,14 @@ const server_config_1 = __importDefault(require("./config/server.config"));
 const routes_1 = __importDefault(require("./routes"));
 const sampleQueueProducer_1 = __importDefault(require("./producer/sampleQueueProducer"));
 const sampleWorker_1 = __importDefault(require("./workers/sampleWorker"));
+const bullBoard_1 = __importDefault(require("./BullBoard ui/bullBoard"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use("/api", routes_1.default);
+app.use("/admin/queues", bullBoard_1.default);
 app.listen(server_config_1.default.PORT, () => {
     console.log(`Server running at http://localhost:${server_config_1.default.PORT}`);
+    console.log(`BullMQ Board UI is available at http://localhost:${server_config_1.default.PORT}/admin/queues`);
     (0, sampleWorker_1.default)("SampleQueue");
     (0, sampleQueueProducer_1.default)("SampleJob", {
         name: "Daniyal Ahmed",
